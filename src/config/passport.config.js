f3a0d3ab17c3a02e1c9826dfe43b2ff3bf71a6fc
Haiver <haiverhercas@gmail.com>
2025-02-05 20:24:45 -0500

    1

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -100,38 +100,33 @@ const initializePassport = () => {
     )
   );
 
-  passport.use(
-    "google",
-    new GoogleStrategy(
-      {
-        clientID: googleClientId,
-        clientSecret: googleClientSecret,
-        callbackURL: "http://localhost:3000/auth/google/callback",
-      },
-      async (request, accesToken, refreshToken, profile, done) => {
-        try {
-          const userFound = await userModel.findOne({
-            email: profile.emails[0]?.value,
-          });
-          if (userFound) {
-            return done(null, userFound);
-          }
-
+  passport.use('google',
+    new GoogleStrategy({
+      clientID: googleClientId,
+      clientSecret:googleClientSecret,
+      callbackURL:'http://localhost:3000/auth/google/callback'
+    },async(request, accesToken, refreshToken,profile,done)=>{
+      try {
+        const userFound = await userModel.findOne({ email: profile.emails[0]?.value });
+        if(userFound){
+          return done(null, userFound)
+        }
+          //si no existe lo crea
           const newUser = {
             first_name: profile.name.givenName || "",
             last_name: profile.name.familyName || "",
             email: profile.emails[0]?.value || "",
-            password: "",
+
+            password: "", // Dejar vacío ya que la autenticación es con Google
           };
 
-          const user = await userModel.create(newUser);
-          return done(null, user);
-        } catch (error) {
-          return done(error);
-        }
+         const user= await userModel.create(newUser)
+         return done(null, user)
+      } catch (error) {
+        return done(error)
       }
-    )
-  );
+    })
+  )
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
